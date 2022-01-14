26d749d94049b0158f60995cfb9f0a28abcef906
JediLuke <luke.taylor@dubber.net>
2022-01-13 20:47:34 -0600

    Fix up MenuBar so that it's generic, but working from QuillEx

--- lib/utils.ex
+++ lib/utils.ex
@@ -32,4 +32,11 @@ defmodule ScenicWidgets.Utils do
       text
     end
   end
+
+  # https://hexdocs.pm/scenic/0.11.0-beta.0/Scenic.Graph.html#bounds/1
+  # def inside?({x, y}, {left, right, top, bottom} = _bounds) do
+  def inside?({x, y}, {left, bottom, right, top} = _bounds) do #TODO update the docs in Scenic itself
+      # top <= y and y <= bottom and left <= x and x <= right
+      bottom <= y and y <= top and left <= x and x <= right
+  end
 end
