ad6fc0c0ebed0cc3566f5db398e53964b9a4bc97
Jason Axelson <axelson@users.noreply.github.com>
2022-08-09 15:13:50 -1000

    Update lib/utils/graph_state.ex
    Co-authored-by: crertel <crertel@users.noreply.github.com>

--- lib/utils/graph_state.ex
+++ lib/utils/graph_state.ex
@@ -1,5 +1,6 @@
 defmodule ScenicWidgets.GraphState do
-  def update_graph(scene, fun) when is_function(fun, 1) do
+  @spec update_graph( Scenic.Scene.t(), fun() ) :: Scenic.Scene.t()
+  def update_graph(%Scenic.Scene{} = scene, fun) when is_function(fun, 1) do
     graph = fun.(scene.assigns.state.graph)
     assign_and_push_graph(scene, scene.assigns.state, graph)
   end
