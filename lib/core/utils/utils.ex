b32571f82984f2cdc67a0c9ea8f121fa1de90e78
Luke T <luke.taylor@dubber.net>
2022-08-12 15:29:25 -0500

    put documentation on same line

--- lib/core/utils/utils.ex
+++ lib/core/utils/utils.ex
@@ -3,8 +3,7 @@ defmodule ScenicWidgets.Utils do
   @doc """
   Checks if a coordinate point (a tuple in the form `{x, y}`)
   is inside a bounding box, defined in the same format as
-  (Scenic.bounds/1)[https://hexdocs.pm/scenic/0.11.0-beta.0/Scenic.Graph.html#bounds/1],
-  `{left, top, right, bottom}`
+  (Scenic.bounds/1)[https://hexdocs.pm/scenic/0.11.0-beta.0/Scenic.Graph.html#bounds/1], `{left, top, right, bottom}`
   """
   def inside?({x, y}, {left, top, right, bottom} = _bounds) do
     # NOTE: Because the y axis starts at zero in the top-left, and
