7534678f0f3db6ab6e8df64b431fb77f07bba32e
Luke T <luke.taylor@dubber.net>
2022-10-23 15:35:23 -0500

    commit FrameBox

--- lib/simple_widgets/frame_box.ex
+++ lib/simple_widgets/frame_box.ex
@@ -1,41 +1,65 @@
 defmodule ScenicWidgets.FrameBox do
-  @moduledoc """
-  FrameBox is a simple component, used during development to quickly
-  see what exact space a %Frame{} occupies.
-  """
-  use Scenic.Component, has_children: false
-  require Logger
-  alias ScenicWidgets.Core.Structs.Frame
+   @moduledoc """
+   FrameBox is a simple component, used during development to quickly
+   see what exact space a %Frame{} occupies.
+   """
+   use Scenic.Component, has_children: false
+   require Logger
+   alias ScenicWidgets.Core.Structs.Frame
 
-  @border_colors [
-    :light_green,
-    :red,
-    :white,
-    :black,
-    :blue
-  ]
+   @border_colors [
+      :light_green,
+      :red,
+      :white,
+      :black,
+      :blue
+   ]
 
-  def validate(%{frame: %Frame{} = _f, color: color} = data) when is_atom(color) do
-    {:ok, data}
-  end
+   @fill_colors [:red, :blue, :green, :yellow]
 
-  def init(scene, args, _opts) do
-    Logger.debug("#{__MODULE__} initializing...")
+   def validate(%{frame: %Frame{} = _f, color: color} = data) when is_atom(color) do
+      {:ok, data}
+   end
 
-    init_graph =
-      Scenic.Graph.build()
-      |> Scenic.Primitives.rect(args.frame.size, fill: args.color, translate: args.frame.pin)
-      |> Scenic.Primitives.rect(args.frame.size,
-        stroke: {10, Enum.random(@border_colors)},
-        translate: args.frame.pin
+   def validate(%{frame: %Frame{} = _f} = data) do
+      validate(data |> Map.merge(%{color: Enum.random(@fill_colors)}))
+   end
+
+   def init(scene, args, _opts) do
+      Logger.debug("#{__MODULE__} initializing...")
+
+      init_graph =
+         Scenic.Graph.build()
+         |> draw(args)
+
+      init_scene =
+         scene
+         |> assign(graph: init_graph)
+         |> assign(frame: args.frame)
+         |> push_graph(init_graph)
+
+      {:ok, init_scene}
+   end
+
+   @doc """
+   This function is static, can be added to any normal graph
+   """
+   def draw(graph, %{frame: %Frame{} = frame, color: color}) do
+
+      # border_stroke = 10
+      graph
+      |> Scenic.Primitives.group(
+         fn graph ->
+            graph
+            |> Scenic.Primitives.rect(frame.size,
+               fill: color,
+               translate: frame.pin
+            )
+            # |> Scenic.Primitives.rect(frame.size,
+            #    # stroke: {border_stroke, color},
+            #    translate: frame.pin
+            # )
+         end
       )
-
-    init_scene =
-      scene
-      |> assign(graph: init_graph)
-      |> assign(frame: args.frame)
-      |> push_graph(init_graph)
-
-    {:ok, init_scene}
-  end
+   end
 end
